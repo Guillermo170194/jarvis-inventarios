3a25b40b9ff47ee42f81022511440bb932616bfe
guillermo ortega <memo170194@gmail.com>
2026-05-15 20:46:57 -0600

    Fix markdown tabs

--- jarvis_inventarios.py
+++ jarvis_inventarios.py
@@ -666,14 +666,14 @@ if menu == "📎 Documentos":
             "✅ Documento cargado correctamente"
         )
 
-	st.markdown(
-	    f"""
-	    <a href="{archivo_url}" target="_blank">
-	        📂 Descargar documento
-	    </a>
-	    """,
-	    unsafe_allow_html=True
-	)
+        st.markdown(
+            f"""
+            <a href="{archivo_url}" target="_blank">
+                📂 Descargar documento
+            </a>
+            """,
+            unsafe_allow_html=True
+        )
 	    elif archivo_doc and not fecha_oficio:
 
         st.warning(
